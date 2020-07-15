9a2cc07045db251b9176ff6748660d67d29f44b4
fackman <1779715457@qq.com>
2020-07-15 22:35:37 +0800

    dele

--- Student_Info.py
+++
@@ -1,9 +0,0 @@
-from xml.dom.minidom import parse
-
-StudentInfoXml = parse(r"info.xml")
-
-StudentInfo = StudentInfoXml.documentElement
-
-Students = StudentInfo.getElementsByTagName("student")
-
-print(Students[0].getAttribute("name"))
